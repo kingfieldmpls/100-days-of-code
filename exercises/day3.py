c41b6ef59375aef1ac020bdbcba7771fb6af4ef2
Rob Martin <rob@rankcrankers.com>
2018-08-01 00:17:28 -0500

    Day 4 push

--- exercises/day3.py
+++ exercises/day3.py
@@ -1,6 +1,6 @@
 import socket
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock = socket.socket()
 mysock.connect(('data.pr4e.org', 80))
 cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
