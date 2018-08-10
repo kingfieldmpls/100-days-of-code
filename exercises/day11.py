40b21ab0478c728a482846c7679ee53e1e8bc4ed
Rob Martin <rob@rankcrankers.com>
2018-08-09 21:48:46 -0500

    day 12 push

--- exercises/day11.py
+++ exercises/day11.py
@@ -43,7 +43,6 @@ for entry in json_data:
 
     name = entry[0];
     title = entry[1];
-    role = entry[2];
 
     print((name, title))
 
@@ -58,7 +57,8 @@ for entry in json_data:
     course_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR REPLACE INTO Member
-        (user_id, course_id, role) VALUES ( ?, ?, ? )''',
-        ( user_id, course_id, role ) )
+        (user_id, course_id) VALUES ( ?, ? )''',
+        ( user_id, course_id ) )
 
     conn.commit()
+
