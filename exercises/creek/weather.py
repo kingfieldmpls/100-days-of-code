892bce3a1b2232e15458cfa8225715751809cc4e
Rob Martin <rob@rankcrankers.com>
2018-08-14 22:32:59 -0500

    day 17 push

--- exercises/creek/weather.py
+++ exercises/creek/weather.py
@@ -24,22 +24,41 @@ c.execute('''
 CREATE TABLE IF NOT EXISTS Weather ("Date" DATE PRIMARY KEY, Precip REAL, Temp REAL)''')
 
 noaa_key = os.environ['NOAA']
-
-headers = {'token': noaa_key}
-payload = {
-    'stationid': 'GHCND:USC00214884', 'datasetid': 'GHCND',
-    'datatypeid': ['PRCP', 'TMAX'], 'startdate': '2018-07-01',
-    'enddate': '2018-07-03', 'units': 'standard'
-}
-
-# https://www.ncdc.noaa.gov/cdo-web/api/v2/stations?datatypeid=EMNT&datatypeid=EMXT&datatypeid=HTMN
-
+offset = 1
+limit = 250
+count = 0
+startdate = '2018-08-01'
+enddate = '2018-08-31'
 url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data'
 
-r = requests.get(url, headers = headers, params = payload)
-
 # Time to write to the DB - might need to handle pagination
+print("Getting report ... ")
+while count is 0 or offset <= count:
+	headers = {'token': noaa_key}
+	payload = {
+	'stationid': 'GHCND:USC00214884', 'datasetid': 'GHCND',
+	'datatypeid': ['PRCP', 'TMAX'], 'startdate': startdate,
+	'enddate': enddate, 'units': 'standard', 'limit': limit,
+	'offset': offset
+	}
+    
+	r = requests.get(url, headers = headers, params = payload)
+	data = json.loads(r.text)
+	count = data['metadata']['resultset']['count']
 
-data = json.loads(r.text)
+	print(f'Grabbing {offset} through {offset + limit - 1} of {count} records')
 
-print(json.dumps(data, indent=4))
+	for result in data['results']:
+		if "PRCP" in result['datatype']:
+			date = result['date']
+			precip = result['value']
+			c.execute('REPLACE INTO Weather ("Date", Precip) VALUES (?,?)',(date[:10], precip))
+		else:
+			date = result['date']
+			temp = result['value']
+			c.execute('UPDATE Weather SET Temp = ? WHERE "Date" = ?', (temp, date[:10]))
+
+	offset += limit
+
+conn.commit()
+c.close()
