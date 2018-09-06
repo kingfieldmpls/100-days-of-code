1b45359dec34a7af8084d64410b4667cb58bd352
Rob Martin <rob@rankcrankers.com>
2018-09-05 23:04:15 -0500

    days 36-38 push

---
+++ exercises/creek/procedural/test_noaa_historical.py
@@ -0,0 +1,14 @@
+import unittest
+import noaa_historical
+
+
+class TestNOAA(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_setupRequest(self):
+        result = 
